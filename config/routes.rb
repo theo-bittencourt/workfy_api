d993cbbdaaf17154a9a1dad70ab8851414bc0db5
Theo Bittencourt <theodrums@gmail.com>
2015-04-22 23:52:59 -0300

    Add auth to Sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   mount API::Root => '/'
 
   require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 end
 
